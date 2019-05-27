93a9e6d73ce8228a109cfcf219a7759be91d80d2
vagrant <vagrant@homestead>
2019-05-27 10:18:02 +0000

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('ver
 Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+

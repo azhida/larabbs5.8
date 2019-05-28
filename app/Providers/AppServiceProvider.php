7b79cd3eeea4f685978d7549d36780654c8320b6
vagrant <vagrant@homestead>
2019-05-28 17:10:26 +0000

    侧边栏链接

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,10 +26,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 	{
+        Schema::defaultStringLength(191);
+
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
-
-        Schema::defaultStringLength(191);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
     }
 }

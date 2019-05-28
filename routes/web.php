71f63ba99fe6579083e594bc15cb36752e3dc235
vagrant <vagrant@homestead>
2019-05-28 17:39:31 +0000

    用户最后活跃时间

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
 
 // 用户身份验证相关的路由

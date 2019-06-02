967307e28f3b486c9572007c2a49b1afd902e322
vagrant <vagrant@homestead>
2019-06-02 16:20:29 +0000

    replies index

--- routes/api.php
+++ routes/api.php
@@ -39,6 +39,15 @@ $api->version('v1', [
         // 分类接口
         $api->get('categories', 'CategoriesController@index')
             ->name('api.categories.index');
+        // 某个用户发布的话题
+        $api->get('users/{user}/topics', 'TopicsController@userIndex')
+            ->name('api.users.topics.index');
+        // 话题回复列表
+        $api->get('topics/{topic}/replies', 'RepliesController@index')
+            ->name('api.topics.replies.index');
+        // 某个用户的回复列表
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
 
 
         // 需要 token 验证的接口
@@ -70,9 +79,6 @@ $api->version('v1', [
             // 话题详情
             $api->get('topics/{topic}', 'TopicsController@show')
                 ->name('api.topics.show');
-            // 某一用户的话题列表
-            $api->get('users/{user}/topics', 'TopicsController@userIndex')
-                ->name('api.users.topics.index');
             // 发布回复
             $api->post('topics/{topic}/replies', 'RepliesController@store')
                 ->name('api.topics.replies.store');
